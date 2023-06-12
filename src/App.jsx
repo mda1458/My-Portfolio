d32deb3130f72f70757fb4357cb838835208f706
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-06-12 17:43:43 +0500

    cmopleted the nav fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { About, Footer, Header, Work, Skills, Testimonials } from './containers'
 import { Navbar } from './components'
 
+import "./App.scss"
+
 const App = () => {
   return (
     <div className="app">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,54 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 import "./Navbar.scss"
+import { images } from "../../constants"
+import { GiHamburger } from "react-icons/gi";
+import { GrFormClose } from "react-icons/gr";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const navlinks = ["home", "about", "work", "skills", "contact"];
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {navlinks.map((item, index) => (
+          <li key={index} className="app__flex p-text">
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+          <GiHamburger onClick={() => setToggle(true)} />
+          {
+            toggle && (
+              <motion.div
+                whileInView={{ x: [300, 0]}}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+              >
+
+                <GrFormClose onClick={() => setToggle(false)} />
+                <ul>
+                {navlinks.map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                  </li>
+                ))}
+                </ul>
+
+              </motion.div>
+            )
+          }
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
