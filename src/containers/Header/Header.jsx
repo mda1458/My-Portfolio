841772aee1f8e3ee55b5ba07976151dc04d3ad33
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-06-13 08:06:25 +0500

    completed header

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -21,7 +21,6 @@ const Header = () => {
     images.python,
     images.redux,
     images.sass,
-    images.bootstrap,
   ]
 
   return (
@@ -36,7 +35,7 @@ const Header = () => {
             <span>👋🏻</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, {"I'm"}</p>
-              <h1 className="head-text">Muhammad Danish Azeem</h1>
+              <h1 className="head-text">Danish</h1>
             </div>
           </div>
 
