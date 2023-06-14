2e8b708c9269b4f6f8e65bb0c20c94793929cb59
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-06-14 07:41:01 +0500

    integrated sanity

---
+++ src/client.js
@@ -0,0 +1,19 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+const token = import.meta.env.VITE_SANITY_TOKEN;
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID;
+console.log(token, projectId);
+export const client = createClient({
+  projectId,
+  dataset: "production",
+  apiVersion: "2022-02-01",
+  useCdn: true,
+  token,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => {
+    return builder.image(source);
+}
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -4,29 +4,21 @@ import { motion } from "framer-motion"
 import { images } from "../../constants"
 import "./About.scss"
 
+import { client, urlFor } from "../../client"
+
 const About = () => {
-  const abouts = [
-    {
-      title: "Web Design",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      img: images.about01
-    },
-    {
-      title: "Web Development",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      img: images.about02
-    },
-    {
-      title: "App Development",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      img: images.about03
-    },
-    {
-      title: "SEO",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      img: images.about04
-    },
-  ]
+  const [abouts, setAbouts] = useState([])
+
+  const getimgUrl = (imgUrl) => {
+    return urlFor(imgUrl).url()
+  }
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((abouts) => setAbouts(abouts))
+  }, [])
+
+
   return (
     <>
       <h2 className="head-text">
@@ -42,7 +34,7 @@ const About = () => {
             transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
           >
-            <img src={about.img} alt="" />
+            <img src={getimgUrl(about.imgUrl)} alt="" />
             <h2 className="bold-text" style={{marginTop: 20}}>
               {about.title}
             </h2>
