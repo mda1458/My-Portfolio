82bb4bfd735c85216eaae1aca20b844b7130dfc2
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-06-19 20:44:11 +0500

    changed link

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -2,13 +2,15 @@
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonial", "contact"].map(
+      {["home", "about", "skills", "work", "testimonial", "contact"].map(
         (item, index) => (
           <a
             key={index}
             href={`#${item}`}
             className="app__navigation-dot"
-            style={{ background: active === item ? "var(--secondary-color)" : "" }}
+            style={{
+              background: active === item ? "var(--secondary-color)" : "",
+            }}
           />
         )
       )}
