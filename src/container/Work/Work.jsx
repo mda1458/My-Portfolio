63ad56d206c239fb2777c14574df39265e1f1bcb
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-06-20 18:06:55 +0500

    some changes in work structure

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -16,6 +16,8 @@ const Work = () => {
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
+      data.sort((a, b) => new Date(b._createdAt) - new Date(a._createdAt));
+      console.log(data);
       setWorks(data);
       setFilterWork(data);
     });
@@ -38,14 +40,25 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> Section
+      </h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {[
+          "Programming",
+          "Web App",
+          "Mobile App",
+          "Desktop App",
+          "AI",
+          "All",
+        ].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
           >
             {item}
           </div>
@@ -59,21 +72,22 @@ const Work = () => {
       >
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
-            <div
-              className="app__work-img app__flex"
-            >
+            <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
-
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -83,7 +97,7 @@ const Work = () => {
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -95,10 +109,29 @@ const Work = () => {
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
+              </div>
+
+              <div className="app__work-techs">
+                {work.techs &&
+                  work.techs.map((tech, index) => (
+                    <motion.div
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0 }}
+                      className="app__work-tech"
+                      key={index}
+                    >
+                      <div>
+                        {tech.tag}
+                      </div>
+                      <img src={urlFor(tech.icon)} alt={work.name} />
+                    </motion.div>
+                  ))}
               </div>
             </div>
           </div>
